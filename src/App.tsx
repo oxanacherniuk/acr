de0bf3c9aa6cb283d526dac1d6e3a7acd828d54c
Pavel545 <p_levin00@mail.ru>
2025-08-26 14:58:38 +0400

    26.08.25

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,16 @@ import {
 
 import AppLayout from './layout/environment';
 import NotFound from './pages/NotFound';
-// Ленивые импорты с корректными путями
 
+// Ленивые импорты с корректными путями
 const HomePage = lazy(() => import("./pages/index"));
 const AmentiesPage = lazy(() => import("./pages/amenities"));
 const DevelopersPage = lazy(() => import("./pages/developers"));
 const PrivacyPolicy = lazy(() => import("./pages/PrivacyPolicy"));
 const DeveloperPage = lazy(() => import("./pages/developerPage"));
 
-// потом вынести и вообще нормальный сделать 
 const LoadingSpinner = () => {
-  return <div>Загрузка...</div>
+  return <div>Загрузка...</div>;
 }
 
 const AppRoutes = () => {
@@ -29,59 +28,55 @@ const AppRoutes = () => {
         {
           index: true,
           element: (
-            <Suspense fallback={<LoadingSpinner  />}>
-              <HomePage  />
+            <Suspense fallback={<LoadingSpinner />}>
+              <HomePage />
             </Suspense>
           ),
         },
         {
           path: "amenties",
           element: (
-            <Suspense fallback={<LoadingSpinner  />}>
-              <AmentiesPage  />
+            <Suspense fallback={<LoadingSpinner />}>
+              <AmentiesPage />
             </Suspense>
           ),
         },
-           {
+        {
           path: "developers",
           children: [
             {
               index: true,
               element: (
-                <Suspense fallback={<LoadingSpinner  />}>
-              <DevelopersPage  />
-            </Suspense>
+                <Suspense fallback={<LoadingSpinner />}>
+                  <DevelopersPage />
+                </Suspense>
               ),
             },
             {
               path: ":id",
               element: (
-                <Suspense fallback={<LoadingSpinner  />}>
-                  <DeveloperPage  />
+                <Suspense fallback={<LoadingSpinner />}>
+                  <DeveloperPage />
                 </Suspense>
               ),
             },
           ],
-         
         },
-           {
+        {
           path: "soglasie-obrabotka-pers-dannih",
           element: (
-            <Suspense fallback={<LoadingSpinner  />}>
-              <PrivacyPolicy  />
+            <Suspense fallback={<LoadingSpinner />}>
+              <PrivacyPolicy />
             </Suspense>
           ),
         },
-          
-        {
-          path: "*",
-          element: (
-            <Suspense fallback={<LoadingSpinner  />}>
-              <NotFound />
-            </Suspense>
-          ),
-        },
+       
       ],
+    },
+    // Добавьте этот маршрут для обработки 404
+    {
+      path: "*",
+      element: <NotFound />,
     },
   ]);
 
